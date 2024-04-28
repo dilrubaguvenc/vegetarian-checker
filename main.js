9182167ec3ba37d220a0373180ad5759c9de46d9
Dilruba Guvenc Lorencio <dilruba.guvenc@gmail.com>
2024-04-28 17:39:09 -0400

    completed vegetarian checker

---
+++ main.js
@@ -0,0 +1,64 @@
+function getFetch(){
+    let inputVal = document.getElementById("barcode").value
+
+    if(inputVal.length !==12){
+        alert(`Please ensure that barcode is 12 characters`)
+        return;
+    }
+
+    const url = `https://world.openfoodfacts.org/api/v3/product/${inputVal}.json`
+
+    fetch(url)
+        .then(res => res.json()) // parse response as JSON
+        .then(data => {
+            console.log(data)
+            if(data.status === "success_with_warnings"){
+            const item = new ProductInfo(data.product)
+            item.showInfo()
+            item.listIngredients()
+            } else if (data.status === "failure"){
+                alert(`Product ${inputVal} not found. Please try another.`)
+            }
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        });
+  }
+  
+  class ProductInfo {
+    constructor(productData){ // I'm passing in data.product
+        this.name = productData.product_name
+        this.ingredients = productData.ingredients
+        this.image = productData.image_url
+    }
+    
+    showInfo() {
+        document.getElementById('product-img').src = this.image
+        document.getElementById('product-name').innerText = this.name
+    }
+
+    listIngredients(){
+        let tableRef = document.getElementById('ingredient-table')
+        for(let i = 1; i < tableRef.rows.length;){
+            tableRef.deleteRow(i);
+        }
+        if(!(this.ingredients == null)) {
+        for(let key in this.ingredients){
+            let newRow = tableRef.insertRow(-1)
+            let newICell = newRow.insertCell(0)
+            let newVCell = newRow.insertCell(1)
+            let newIText = document.createTextNode(this.ingredients[key].text)
+
+            let vegStatus = !(this.ingredients[key].vegetarian) ? 'unknown' : this.ingredients[key].vegetarian
+            let newVText = document.createTextNode(vegStatus)
+            newICell.appendChild(newIText)
+            newVCell.appendChild(newVText)
+            if(vegStatus === 'no'){
+                newVCell.classList.add('non-veg-item')
+            }else if(vegStatus === 'unknown' || vegStatus === 'maybe'){
+                newVCell.classList.add('unknown-maybe-item')
+             }
+         }
+     }
+    }
+  }
